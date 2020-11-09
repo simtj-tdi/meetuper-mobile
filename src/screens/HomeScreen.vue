ba0b5763308828cb220f21c7a9f12d9395c0c95e
simtj-tdi <60678325+simtj-tdi@users.noreply.github.com>
2020-11-10 00:58:14 +0900

    commit

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,16 +1,29 @@
 <template>
   <scroll-view>
+    <!--
     <nb-list>
-      <nb-list-item v-for="todo in todos">
-        <nb-text>{{todo.title}}</nb-text>
+      <nb-list-item v-for="meetup in meetups">
+        <nb-text>{{meetup.title}}</nb-text>
       </nb-list-item>
     </nb-list>
+    -->
+
+      <nb-text class="header-1">Featured Meetups</nb-text>
+      <!-- Separate This -->
+      <MeetupCard v-for="meetup in meetups"
+                  :meetup="meetup"
+                  :navigateToDetail="goToMeetupDetail"
+                  :key="meetup._id" />
+      <!-- Separate This -->
   </scroll-view>
 </template>
 
 <script>
-
+import MeetupCard from '@/components/MeetupCard'
 export default {
+  components: {
+    MeetupCard
+  },
   props: {
     navigation: {
       type: Object
@@ -18,23 +31,37 @@ export default {
   },
   data() {
     return {
-      title: 'Home Screen!'
+      title: 'Home Screen!',
     }
   },
   computed: {
     // 5. After mutation we are getting here new dada
     todos () {
       return this.$store.state.todos
+    },
+    meetups () {
+      return this.$store.state.meetups.items
     }
   },
   created () {
     // 1. dispatching fetchTodos, this will call action fetchTodos
     this.$store.dispatch('fetchTodos')
+    this.$store.dispatch('meetups/fetchMeetups')
   },
   methods: {
     goToScreen1 () {
       this.navigation.navigate('ScreenOne')
+    },
+    goToMeetupDetail () {
+      this.navigation.navigate('Meetup')
     }
   }
 }
 </script>
+<style>
+  .header-1 {
+    font-size: 23px;
+    padding: 20px;
+    font-weight: bold;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,18 @@ import vuex from 'vuex'
 import Vue from 'vue-native-core'
 import axios from 'axios'
 
+import meetups from './modules/meetups'
+
 Vue.use(vuex)
 
+
 export default new vuex.Store({
+    modules: {
+        meetups
+    },
     // Like data in component. We are keeping our data in the state
     state: {
-        todos: []
+        todos: [],
     },
     // Like computed properties in componen.
     // You can use getters to access state in the store
@@ -18,21 +24,21 @@ export default new vuex.Store({
     // To perform actions that usualy results in data
     actions: {
         // 2. We are getting here from dispatch of action in homeScreen
-        fetchTodos () {
-            axios.get('https://jsonplaceholder.typicode.com/todos')
+        fetchTodos ({commit, state}) {
+            return axios.get('https://jsonplaceholder.typicode.com/todos')
                 .then(res => {
                     const todos = res.data
                     // 3. Cmmiting setTodos mutation
-                    this.commit('setTodos', todos)
-                    return this.state.todos
+                    commit('setTodos', todos)
+                    return state.todos
                 })
-        }
+        },
     },
     // Like methods in component. To save data to the state.
     mutations: {
         setTodos (state, todos) {
             // 4. We are seeting data to our sate in reactive way
             Vue.set(state, 'todos', todos)
-        }
+        },
     }
 })
